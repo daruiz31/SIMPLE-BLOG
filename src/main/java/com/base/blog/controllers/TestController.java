acceaf65d61ebb32774f32bfb790e7d7a9dfdae8
diego.ruiz.cellvoz <diego.ruiz@cellvoz.com.co>
2022-09-07 16:01:40 -0500

    DELETE: Se elimina función de prueba login

--- src/main/java/com/base/blog/controllers/TestController.java
+++ src/main/java/com/base/blog/controllers/TestController.java
@@ -4,34 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.base.blog.dtos.ResponseBlog;
-import com.base.blog.services.ILoginService;
 import com.base.blog.services.ITestService;
 
 @RestController
-@RequestMapping(path = "/test")
+@RequestMapping(path = "/simpleBlog/v1/test")
 public class TestController {
 
 	@Autowired
 	private ITestService iTestService;
 
-	@Autowired
-	private ILoginService iLoginService;
-
 	@GetMapping("/ok")
 	@ResponseStatus(HttpStatus.ACCEPTED)
 	public ResponseBlog<String> test() {
 		return iTestService.test();
-	}
-
-	@GetMapping("/sesion/iniciar")
-	@ResponseStatus(HttpStatus.OK)
-	public ResponseBlog<String> login(@RequestParam String email, @RequestParam String password) {
-		return iLoginService.login(email, password);
 	}
 
 }
