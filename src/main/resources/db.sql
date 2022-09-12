728fea80a4bd487dfd411e6c22e9fac9acd751e3
diego.ruiz.cellvoz <diego.ruiz@cellvoz.com.co>
2022-09-12 17:10:03 -0500

    REFACTOR: Ajustes para base de datos

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -9,7 +9,7 @@ CREATE TABLE `roles` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='roles for simple blog users';
 CREATE TABLE `users` (
   `idusers` bigint NOT NULL AUTO_INCREMENT,
-  `name` int NOT NULL,
+  `name` varchar(45)  NOT NULL,
   `surname` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
   `password` varchar(99) NOT NULL,
@@ -36,6 +36,5 @@ INSERT INTO `simpleblog`.`roles`
 `enabled`)
 VALUES
 ('2', 'ADMINISTRATOR', 'application administrator', '1');
-
 
 
