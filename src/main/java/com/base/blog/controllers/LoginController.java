fac9799759bf6f776b693d0e555cb16fc248ab79
diego.ruiz.cellvoz <diego.ruiz@cellvoz.com.co>
2022-09-21 18:25:14 -0500

    ADD: Cors origin *

--- src/main/java/com/base/blog/controllers/LoginController.java
+++ src/main/java/com/base/blog/controllers/LoginController.java
@@ -6,6 +6,7 @@ import javax.validation.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +20,7 @@ import com.base.blog.services.ILoginService;
 
 @RestController
 @RequestMapping(path = "/simpleBlog/v1/login")
+@CrossOrigin("*")
 public class LoginController {
 
 	@Autowired
--- src/main/java/com/base/blog/controllers/TestController.java
+++ src/main/java/com/base/blog/controllers/TestController.java
@@ -2,6 +2,7 @@ package com.base.blog.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,6 +13,7 @@ import com.base.blog.services.ITestService;
 
 @RestController
 @RequestMapping(path = "/simpleBlog/v1/test")
+@CrossOrigin("*")
 public class TestController {
 
 	@Autowired
