cbb4b4a89387a5a259f753f6cf42e671ae26840e
diego.ruiz.cellvoz <diego.ruiz@cellvoz.com.co>
2022-09-21 19:03:43 -0500

    ADD: Cross origin *

--- src/main/java/com/base/blog/controllers/RegisterController.java
+++ src/main/java/com/base/blog/controllers/RegisterController.java
@@ -6,6 +6,7 @@ import javax.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import com.base.blog.services.IRegisterService;
 
 @RestController
 @RequestMapping(path = "/simpleBlog/v1/register")
+@CrossOrigin("*")
 public class RegisterController {
 
 	@Autowired
--- src/main/java/com/base/blog/controllers/ReviewsController.java
+++ src/main/java/com/base/blog/controllers/ReviewsController.java
@@ -8,6 +8,7 @@ import javax.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +25,7 @@ import com.base.blog.services.IReviewsService;
 
 @RestController
 @RequestMapping(path = "/simpleBlog/v1/review")
+@CrossOrigin("*")
 public class ReviewsController {
 
 	@Autowired
