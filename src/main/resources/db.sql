0ff819e77894f4bb2f6487395e6cb598b94723ad
diego.ruiz.cellvoz <diego.ruiz@cellvoz.com.co>
2022-09-30 09:44:25 -0500

    ADD: Configuración de producción

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,6 +1,3 @@
-CREATE DATABASE `simpleblog` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `simpleblog`;
-
 CREATE TABLE `roles` (
   `idroles` bigint NOT NULL AUTO_INCREMENT,
   `role` varchar(45) NOT NULL,
@@ -8,7 +5,7 @@ CREATE TABLE `roles` (
   `enabled` tinyint NOT NULL,
   PRIMARY KEY (`idroles`),
   UNIQUE KEY `role_UNIQUE` (`role`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='roles for simple blog users';
+);
 
 CREATE TABLE `users` (
   `idusers` bigint NOT NULL AUTO_INCREMENT,
@@ -22,7 +19,7 @@ CREATE TABLE `users` (
   UNIQUE KEY `email_UNIQUE` (`email`),
   KEY `user_idrol_fk_idx` (`idrol`),
   CONSTRAINT `user_idrol_fk` FOREIGN KEY (`idrol`) REFERENCES `roles` (`idroles`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='registered users';
+);
 
 CREATE TABLE `reviews` (
   `idreviews` bigint NOT NULL AUTO_INCREMENT,
@@ -33,10 +30,10 @@ CREATE TABLE `reviews` (
   PRIMARY KEY (`idreviews`),
   KEY `iduser_review_fk_idx` (`iduser`),
   CONSTRAINT `review_iduser_fk` FOREIGN KEY (`iduser`) REFERENCES `users` (`idusers`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 
-INSERT INTO `simpleblog`.`roles`
+INSERT INTO `roles`
 (`idroles`,
 `role`,
 `description`,
@@ -44,7 +41,7 @@ INSERT INTO `simpleblog`.`roles`
 VALUES
 ('1', 'USER', 'registered users', '1');
 
-INSERT INTO `simpleblog`.`roles`
+INSERT INTO `roles`
 (`idroles`,
 `role`,
 `description`,
@@ -52,7 +49,7 @@ INSERT INTO `simpleblog`.`roles`
 VALUES
 ('2', 'ADMINISTRATOR', 'application administrator', '1');
 
-INSERT INTO `simpleblog`.`roles`
+INSERT INTO `roles`
 (`idroles`,
 `role`,
 `description`,
@@ -60,7 +57,7 @@ INSERT INTO `simpleblog`.`roles`
 VALUES
 ('3', 'TEST', 'simple role for test', '1');
 
-INSERT INTO `simpleblog`.`users`
+INSERT INTO `users`
 (`idusers`,
 `name`,
 `surname`,
@@ -75,5 +72,3 @@ VALUES
 '7c4a8d09ca3762af61e59520943dc26494f8941b',
 1,
 3);
-
-
